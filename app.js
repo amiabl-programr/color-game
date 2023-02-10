a3e76df52d41c7c016d19471233d18cd1ab8cb86
AyoOlu1 <oluwayemivictor15@gmail.com>
2023-02-10 11:40:56 +0100

    generate random colors on squares

--- app.js
+++ app.js
@@ -2,19 +2,18 @@
 var numGrids = 6;
 
 // getPickedColor();
-
+var rgb;
 var headerBg = document.querySelector(".title");
 var refreshGameBtn = document.querySelector(".refresh");
 var easyBtn = document.querySelector(".easy");
 var hardBtn = document.querySelector(".hard");
 var colorHeading = document.querySelector("h1");
-var grids = document.querySelectorAll(".grids");
+var squares = document.querySelectorAll(".grids");
 var i;
-var rgb;
-console.log(grids);
+console.log(squares);
 // ------functions------
 // Generate random numbers
-getRandomColors();
+// getRandomColors();
 getPickedColor();
 generateRandomGridColors();
 
@@ -23,8 +22,9 @@ function getRandomColors() {
   let g = Math.round(Math.random() * 255);
   let b = Math.round(Math.random() * 255);
   rgb = `${r}, ${g}, ${b}`;
-
+  return rgb;
 }
+
 // set header background color
 headerBg.style.backgroundColor = `rgb(${rgb})`;
 
@@ -33,27 +33,28 @@ colorHeading.textContent = `RGB(${rgb})`
 
 function reset() { }
 function getPickedColor() {
-  // loop through the grids and add a click listener
+  // loop through the squares and add a click listener
 
-  for (i = 0; i < grids.length; i++) {
-    // console.log(grids[i].style.backgroundColor)
-    grids[i].addEventListener("click", function () {
+  // for (i = 0; i < squares.length; i++) {
+  //   // console.log(squares[i].style.backgroundColor)
+  //   squares[i].addEventListener("click", function () {
 
-      // this.style.backgroundColor = `rgb(${rgb})`
-    }
+  //     // this.style.backgroundColor = `rgb(${rgb})`
+  //   }
 
-    );
+  //   );
 
-  }
+  // }
 
 }
-
+// console.log(squares[0])
 function generateRandomGridColors() {
-  for (i = 0; i < grids.length; i++) {
-    let randomColors = Math.random(rgb);
-    console.log(randomColors)
-    grids[i].style.backgroundColor = `rgb(${randomColors})`
+  var randomSquare = Math.round(Math.random() * 6);
+  console.log(randomSquare);
+  for (i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = `rgb(${getRandomColors()})`;
   }
+
 }
 
 // add event listener to buttons
